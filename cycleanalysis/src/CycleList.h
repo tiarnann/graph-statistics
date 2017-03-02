c91f3ad9251280e98dd4be69e0c099553517457d
Tiarnan McGrath <mcgratti@tcd.ie>
2017-03-02 11:13:39 +0000

    More refactoring...

--- cycleanalysis/src/CycleList.h
+++ cycleanalysis/src/CycleList.h
@@ -3,4 +3,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "Cycle.h"
+
+
+//-----------------------------------------------------------------------------------------------
+//LISTNODES & LISTS OF LISTNODES
+//Structures
+struct _CycleList{
+	int size;
+    Cycle *head;
+    Cycle *tail;
+} typedef CycleList;
+
+//Constructors
+
+CycleList *createCycleList(){
+    CycleList *result = malloc(sizeof(CycleList));
+    result -> head = NULL;
+    result -> tail = NULL;
+    result.size = 0;
+    return result;
+};
+
+// Functions
+void appendCycle (Cycle *cycle, CycleList *list){
+	//append cycle to list
+    if (list -> head == NULL){
+		list -> head = cycle;
+		list -> tail = cycle;
+    }
+	else {
+		list -> tail -> next = cycle;
+		list -> tail = cycle;
+	}	
+	list.size += 1;
+}
+
+void printCycleList(CycleList *list) {
+	if(list == NULL) return;
+
+	CycleList *current = list->head;
+
+	while(current != NULL){
+		printCycle(current);
+		current = current-> next;
+	}
+}
+
+void freeCycleList(CycleList *list)
+{
+	Cycle* current = cycle -> head;
+	Cycle* next;
+	while(current != NULL){
+		next = current -> next;
+		freeCycle(current);
+		current = next; 
+	}
+	freeCycle(cycle);
+} 
+
 #endif CYCLE_H
