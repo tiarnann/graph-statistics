79a401fc83c5ca211cc9b6a182cef66cc5281f6a
Joanna Whittam <whittamj@lg12l32.scss.tcd.ie>
2017-03-30 16:22:20 +0100

    Cleaned up butt_list_cycle_nodes.R and added comments throughout.

--- cycleanalysis/R/butt_list_cycle_nodes.R
+++ cycleanalysis/R/butt_list_cycle_nodes.R
@@ -1,23 +1,25 @@
-cycle.mostImportantNodes <- function(cycles, count) {
-  mostImportantNodes <- as.matrix(count)
-  theList <- list()   #indexes are the nodes, values are the amount of times they reoccur
 
-  for(z in 1: length(adjacencyMat[1, ])){  #fill this list with 0 values
+# cycle.mostImportantNodes - Returns the 3 most important nodes in the graph
+# Parameters: cycles = list of cycles
+#             count = number of cycles
+# Returns:    mostImportantNodes = list of the 3 most important nodes
+cycle.mostImportantNodes <- function(cycles, count) {
+  # create a list of counts with a value for each node
+  theList <- list()   # indexes are the nodes, values are the amount of times they reoccur in the given cycles
+
+  for(z in 1: length(adjacencyMat[1, ])){  # initialise list to 0
     theList[z] = 0;
   }
-  for( i in 1:count){
-    for(j in 1: length( cycles[[i]])){
-      #print(cycles[[i]][[j]])
-      formatting <- unlist(strsplit(cycles[[i]][[j]], 's', fixed=TRUE))     #convert character values into ints
+  for (i in 1:count){
+    for (j in 1:length(cycles[[i]]) ){
+      formatting <- unlist(strsplit(cycles[[i]][[j]], 's', fixed=TRUE)) # convert character values into ints
       theNumber <- as.numeric(formatting[[2]])
-      #print(theNumber)
-      #print(theList)
-      theList[[theNumber]] = theList[[theNumber]] + 1                              #find the index of the character and increment
+      theList[[theNumber]] = theList[[theNumber]] + 1                   # find the index of the character and increment
     }
-
   }
-  #print(theList)
-  big <- which.max(theList)                                                #taking the highest three indexes and deleting them from the list
+
+  # find the highest three values and delete them from the list
+  big <- which.max(theList)
   theList[which.max(theList)] <- 0
 
   secondBig <- which.max(theList)
@@ -26,36 +28,37 @@ cycle.mostImportantNodes <- function(cycles, count) {
   thirdBig <- which.max(theList)
   theList[which.max(theList)] <- 0
 
+  mostImportantNodes <- as.matrix(3)
   mostImportantNodes[1] <- big
-  mostImportantNodes[2] <-secondBig
+  mostImportantNodes[2] <- secondBig
   mostImportantNodes[3] <- thirdBig
-  #output
+
+  # output
   mostImportantNodes
 }
 
-
-# Calculates the total edge weight of each cycle
+# cycle.edgeWeightTotal - Calculates the total edge weight of each cycle
 # Parameters: cycles = list of cycles
 #             edgeMat = matrix of edges and associated weights with column names "from", "to" and "weight"
 #             count = number of cycles
 # Returns:    edgeWeightTotal = list of total edge weight for each cycle
 cycle.edgeWeightTotal <- function(cycles, edgeMat, count) {
-  # Retrieve indices associated with column names
+  # retrieve indices associated with column names
   columnNames <- as.matrix(colnames(edgeMat))
   for (k in 1:length(columnNames)){
-    if (columnNames[k] == "from"){        fromIdx <- k   }
-    else if (columnNames[k] == "to"){     toIdx <- k     }
+    if (columnNames[k] == "from"){        fromIdx   <- k }
+    else if (columnNames[k] == "to"){     toIdx     <- k }
     else if (columnNames[k] == "weight"){ weightIdx <- k }
   }
 
-  # Initialize and fill list of total edge weights
+  # initialize and fill list of total edge weights
   edgeWeightTotals <- as.matrix(count)
-  # For every cycle
-  for (i in 1:count) {
+  for (i in 1:count) { # for every cycle
     totalWeight <- 0
-    for (j in 1:( length( cycles[[i]] ))  ) {
-      #safe checking for the last pair of nodes
-      if (j == length( cycles[[i]] )) {
+
+    for (j in 1:( length( cycles[[i]] ))  ) { # for every node
+      # read node names
+      if (j == length( cycles[[i]] )) { # safe check for the last pair of nodes
         node1 <- cycles[[i]][j]
         node2 <- cycles[[i]][1]
       }
@@ -64,7 +67,7 @@ cycle.edgeWeightTotal <- function(cycles, edgeMat, count) {
         node2 <- cycles[[i]][j+1]
       }
 
-      #find node pair in edgelist and get weight
+      # find node pair in edgelist matrix and get weight
       foundBoth  <- FALSE
       idx <- 1
       while (!foundBoth && idx<=nrow(edgeMat)) {
@@ -72,43 +75,37 @@ cycle.edgeWeightTotal <- function(cycles, edgeMat, count) {
           totalWeight <- totalWeight +  as.double(edgeMat[idx, weightIdx])
           foundBoth <- TRUE
         }
-        else {
-          idx <- idx + 1
-        }
+        else { idx <- idx + 1 }
       }
     }
     edgeWeightTotals[i] <- totalWeight
   }
-  #Output
+  # output
   edgeWeightTotals
 }
 
-# Calculates the minimum edge weight in each cycle
+# cycle.minimumEdgeWeight - Calculates the minimum edge weight in each cycle
 # Parameters: cycles = list of cycles
 #             edgeMat = matrix of edges and associated weights with column names "from", "to" and "weight"
 #             count = number of cycles
 # Returns:    minimumEdgeWeight = list of the minimum edge weight for every cycle
 cycle.minimumEdgeWeight <- function(cycles, edgeMat, count) {
-  # Retrieve index associated with column names
+  # retrieve index associated with column names
   columnNames <- as.matrix(colnames(edgeMat))
   for (k in 1:length(columnNames)){
-    if (columnNames[k] == "from"){
-      fromIdx <- k
-    }
-    else if (columnNames[k] == "to"){
-      toIdx <- k
-    }
-    else if (columnNames[k] == "weight"){
-      weightIdx <- k
-    }
+    if (columnNames[k] == "from"){        fromIdx   <- k }
+    else if (columnNames[k] == "to"){     toIdx     <- k }
+    else if (columnNames[k] == "weight"){ weightIdx <- k }
   }
 
+  # initialize and fill list of total edge weights
   minimumEdgeWeights <- as.matrix(count)
-  for (i in 1:count) {
+  for (i in 1:count) { # for every cycle
     minWeight <- 0
-    for (j in 1:( length( cycles[[i]] ))  ) {
-      #safe checking for the last pair of nodes
-      if (j == length( cycles[[i]] )) {
+
+    for (j in 1:( length( cycles[[i]] ))  ) { # for every node
+      # read node names
+      if (j == length( cycles[[i]] )) { # safe check for the last pair of nodes
         node1 <- cycles[[i]][j]
         node2 <- cycles[[i]][1]
       }
@@ -117,7 +114,7 @@ cycle.minimumEdgeWeight <- function(cycles, edgeMat, count) {
         node2 <- cycles[[i]][j+1]
       }
 
-      #find node pair in edgelist & get weight
+      # find node pair in edgelist & get weight
       foundBoth  <- FALSE
       idx <- 1
       while (!foundBoth && idx<=nrow(edgeMat)) {
@@ -138,7 +135,7 @@ cycle.minimumEdgeWeight <- function(cycles, edgeMat, count) {
     }
     minimumEdgeWeights[i] <- minWeight
   }
-  #Output
+  # output
   minimumEdgeWeights
 }
 
@@ -146,18 +143,29 @@ cycle.minimumEdgeWeight <- function(cycles, edgeMat, count) {
 #------------------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------------------
 
-#kcycle.list 	- Compute the cycle census of a graph, possibly along with
-#additional information on the inidence of cycles.
-#				- Output list of each node in every cycle
-
+# kcycle.censusExtension - Compute the cycle census of a graph, along with additional cycle information
+#  on the inidence of cycles.
+# Parameters:   dat = adjacency matrix for the graph
+#               edges = matrix of edges and associated weights with column names "from", "to" and "weight"
+#               maxlen = the maximum path/cycle length to evaluate
+#               mode = "digraph" for directed graphs, or "graph" for undirected graphs
+#               tabulate.by.vertex =  logical; should path or cycle incidence counts be tabulated by vertex?
+#               cycle.comembership =  the type of cycle co-membership information to be tabulated, if any. "sum"
+#                                     returns a vertex by vertex matrix of cycle co-membership counts; these are
+#                                     disaggregated by cycle length if "bylength" is used. If "none" is given, no
+#                                     co-membership information is computed
+#	Returns:      cycles = list of nodes in each cycle
+#               edgeWeightTotal = list of total edge weight for each cycle
+#               minimumEdgeWeight = list of the minimum edge weight for every cycle
+#               mostImportantNodes = XXXX
 kcycle.censusExtension<-function(dat, edges, maxlen=3,mode="digraph",tabulate.by.vertex=TRUE,cycle.comembership=c("none","sum","bylength")){
-  #Pre-process the raw input
+  # pre-process the raw input
   dat<-as.edgelist.sna(dat)
   if(is.list(dat))
     return(lapply(dat,kcycle.list,maxlen=maxlen,mode=mode, tabulate.by.vertex=tabulate.by.vertex,cycle.comembership=cycle.comembership))
-  #End pre-processing
 
-  n<-attr(dat,"n")						# n =  the number of nodes
+  # process function parameters
+  n<-attr(dat,"n")	# n is the number of nodes
   if(is.null(maxlen))
     maxlen<-n
   if(maxlen<2)
@@ -172,7 +180,9 @@ kcycle.censusExtension<-function(dat, edges, maxlen=3,mode="digraph",tabulate.by
     directed<-FALSE
   cocycles<-switch(match.arg(cycle.comembership), "none"=0, "sum"=1, "bylength"=2)
 
-  #Generate the data structures for the counts
+  print(vnam)
+
+  # generate the data structures for the counts
   if(!tabulate.by.vertex)
     count<-rep(0,maxlen-1)
   else
@@ -186,49 +196,20 @@ kcycle.censusExtension<-function(dat, edges, maxlen=3,mode="digraph",tabulate.by
   if(is.null(maxlen))
     maxlen<-n
 
-
-  #Calculate the cycle information
+  # calculate the cycle information
   ccen <- .Call("cycleCensusID_R", dat, n, NROW(dat), count, cccount, maxlen, directed, tabulate.by.vertex,cocycles, c(vnam),PACKAGE = "cycleanalysis")
 
-  #Calculate edge weight information
+  # calculate additional information from the cycles found
   edgeMat <- as.matrix(edges, sparse=FALSE)
   edgeWeightTotal <- cycle.edgeWeightTotal(ccen, edgeMat, length(ccen))
   minimumEdgeWeight <- cycle.minimumEdgeWeight(ccen, edgeMat, length(ccen))
-  mostImportantNodes <- cycle.mostImportantNodes(ccen, length(ccen))           #AAAAAAAAAA
-  #Return the result
-  out<-list(cyclecounts=count)
-  out$cycles <- ccen
-  out$edgeWeightTotal <- edgeWeightTotal
-  out$minimumEdgeWeight <- minimumEdgeWeight
-  out$mostImportantNodes <-mostImportantNodes                                 #AAAAAAAAAA
-  return (out)
+  mostImportantNodes <- cycle.mostImportantNodes(ccen, length(ccen))
 
-  # # Coerce the cycle counts into the right form
-  # if(!tabulate.by.vertex){
-  #   count<-ccen$count
-  #   names(count)<-2:maxlen
-  # }else{
-  #   count<-matrix(ccen$count,maxlen-1,n+1)
-  #   rownames(count)<-2:maxlen
-  #   colnames(count)<-c("Agg",vnam)
-  # }
-  # if(cocycles==1){
-  #   cccount<-matrix(ccen$cccount,n,n)
-  #   rownames(cccount)<-vnam
-  #   colnames(cccount)<-vnam
-  # }else if(cocycles==2){
-  #   cccount<-array(ccen$cccount,dim=c(maxlen-1,n,n))
-  #   dimnames(cccount)<-list(2:maxlen,vnam,vnam)
-  # }
-  #
-  #
-  # #Return the result
-  # out<-list(cycle.count=count)
-  # out$vnam <- c("Agg",vnam)
-  # out$cycle.ccenCount<-ccen$count
-  # out$cycle.matrix = outList
-  # #out$cycle.ccenCccount<-ccen$cccount
-  # if(cocycles>0)
-  #   out$cycle.comemb<-cccount
-  # out
+  # output
+  out<-list(cyclecounts=count)
+  out$cycles              <- ccen
+  out$edgeWeightTotal     <- edgeWeightTotal
+  out$minimumEdgeWeight   <- minimumEdgeWeight
+  out$mostImportantNodes  <- mostImportantNodes
+  return (out)
 }
