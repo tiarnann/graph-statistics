3bb69883a60eda6ab6d878f3c2c9a4a3bde6d42c
dywalsh <dywalsh@tcd.ie>
2017-04-03 11:37:24 +0100

    fixed bug in mostImportantNodes - allowing for all node sizes

--- cycleanalysis/run.R
+++ cycleanalysis/run.R
@@ -3,8 +3,8 @@ library('igraph')
 library('statnet')
 
 # Read data from .csv files
-nodes <- read.csv("sample_data/Dataset_nodes_100_300.csv", header=T, as.is=T)
-edges <- read.csv("sample_data/Dataset_edges_100_300.csv", header=T, as.is=T)
+nodes <- read.csv("sample_data/Dataset_nodes_200_500.csv", header=T, as.is=T)
+edges <- read.csv("sample_data/Dataset_edges_200_500.csv", header=T, as.is=T)
 
 # Manipulate data into correct format
 net <- graph_from_data_frame(d=edges, vertices=nodes, directed=T)
@@ -17,3 +17,9 @@ plot(net, edge.arrow.size=0.01, edge.arrow.width=0.001, edge.color="lightpink", 
 # Run kcycle.censusExtension using the adjacency matrix for the graph
 cc <- kcycle.censusExtension(adjacencyMat, edges, maxlen=5, tabulate.by.vertex = FALSE, cycle.comembership = "bylength")
 message(as.character(length(cc$cycles)), " Cycles Found.")
+
+a<-cc$minimumEdgeWeight
+b<-cc$edgeWeightTotal
+c<-cc$mostImportantNodes
+d<-cc$cyclecounts
+
